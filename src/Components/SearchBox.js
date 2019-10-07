c59dd21594e12c905dcae181bdcbc724bd7b159b
Dorin-Ciora <52659995+Dorin-Ciora@users.noreply.github.com>
2019-10-07 18:34:05 +0300

    adding label

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -4,12 +4,14 @@ const SearchBox = ({searchField, searchChange}) => {
     console.log('searchBox')
     return (
         <div className='pa2'>
+        <label>
         <input 
         className ='pa3 ba b--green bg-lightest-blue'
         type='search' 
         placeholder='search robots'
         onChange={searchChange} 
         />
+        </label>
         </div>
     );
 }
