9923dfb5bcb931a060a19d3aafbd5ad6a4ad26f6
Dorin-Ciora <52659995+Dorin-Ciora@users.noreply.github.com>
2019-10-07 17:25:53 +0300

    Updates

---
+++ precache-manifest.ede7d66f87256d8ec513d3d602f9e05d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8b96bda2491d480fba74adda53027c",
+    "url": "/Robo-Friends-Redux/index.html"
+  },
+  {
+    "revision": "1692e2135af89f3e98f9",
+    "url": "/Robo-Friends-Redux/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ec59e221a5f0ac4eebc7",
+    "url": "/Robo-Friends-Redux/static/css/main.18a8cacb.chunk.css"
+  },
+  {
+    "revision": "1692e2135af89f3e98f9",
+    "url": "/Robo-Friends-Redux/static/js/2.96da6c05.chunk.js"
+  },
+  {
+    "revision": "ec59e221a5f0ac4eebc7",
+    "url": "/Robo-Friends-Redux/static/js/main.57ffd955.chunk.js"
+  },
+  {
+    "revision": "19c3be0f1298dd0ec36e",
+    "url": "/Robo-Friends-Redux/static/js/runtime~main.2fc62855.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends-Redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
